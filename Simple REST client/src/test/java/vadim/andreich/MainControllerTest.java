d1175e1813410bf16b9c476a70c7381636a9876c
vadim <storozhukmail@gmail.com>
2022-10-05 19:47:34 +0300

    added after and before test sql scripts

--- Simple REST client/src/test/java/vadim/andreich/MainControllerTest.java
+++ Simple REST client/src/test/java/vadim/andreich/MainControllerTest.java
@@ -1,23 +1,36 @@
 package vadim.andreich;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import vadim.andreich.services.SensorService;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
 @TestPropertySource("/application.yaml")
 @AutoConfigureMockMvc
-public class MainControllerTest {
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "/before.sql")
+@Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "/after.sql")
+class MainControllerTest {
+//JUnit5 is more tolerant regarding the visibilities of Test classes than JUnit4, which required everything to be public.
+//In this context, JUnit5 test classes can have any visibility but private, however,
+// it is recommended to use the default package visibility, which improves readability of code.
 
     @Autowired
     MockMvc mock;
+    @Autowired
+    SensorService sensorService;
 
     @Test
-    public void contextLoads(){
-
+    void contextLoads() {
+        assertThat(mock).isNotNull();
     }
 }
